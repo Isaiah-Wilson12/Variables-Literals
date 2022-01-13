973982b35821e64e5d6313833693c1f3d9c70c2d
Isaiah-Wilson1 <Isaiah.wilson@hgs.hiddengeniusproject.org>
2022-01-13 02:33:05 +0000

    Finished activity Variables

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    String name = "Isaiah";
+    System.out.println(name);
+
+    int myNum = 15;
+    System.out.println(myNum);
+
+    String Job = "Entrepreneur";
+    System.out.println(Job);
+
+    System.out
+        .println("My name is " + name + ". I am " + myNum + " years old. When I grow up I want to be a/an " + Job + ".");
+
+  }
+}
